e89473837a902b8060f8f6b1a7c000ee98ca7fa6
Linus <linushakansson89@gmail.com>
2018-05-15 16:08:17 +0200

    Update service.go

--- service.go
+++ service.go
@@ -6,17 +6,19 @@ import ("github.com/ant0ine/go-json-rest/rest"
 	"net/http"
 	"os")
 func main() {
+	fmt.Println("Init")
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
 	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
 		w.WriteJson(map[string]string{"Body": "Figher is running!"})
 	}))
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), api.MakeHandler()))
-	
+	fmt.Println("Listening")
 	response, err := http.Get("http://ip.jsontest.com/")
 	if err != nil {
 		fmt.Println("Error making GET call")
 	} else {
+		fmt.Println("Get completed")
 	    defer response.Body.Close()
         contents, err := ioutil.ReadAll(response.Body)
 		if err != nil {
