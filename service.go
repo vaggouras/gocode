07dbf98eaf45b45b717aa625bcf44e9de1a361b3
Linus Hakansson <linus.hakansson@oracle.com>
2018-05-15 17:29:32 +0200

    Final Go commit

--- service.go
+++ service.go
@@ -1,18 +1,35 @@
 package main
 import ("github.com/ant0ine/go-json-rest/rest"
-	"log"
 	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"net/http"
 	"os")
 func main() {
-	api := rest.NewApi()
-	api.Use(rest.DefaultDevStack...)
-	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
-		w.WriteJson(map[string]string{"Body": "Figher is running!"})
-	}))
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), api.MakeHandler()))
+	fmt.Println("Fighter initialized")
 	
+	// try making database connection and query
+    db, err := sql.Open("mysql", "user:pass@tcp(ip:port)/databasename")
+    defer db.Close()
+
+    if err != nil {
+        fmt.Println("Failed to connect", err)
+        return
+    }
+	
+	var xCoordinate int
+	var yCoordinate int
+	err = db.QueryRow("SELECT * from Sample").Scan(&xCoordinate, &yCoordinate)
+	switch {
+	case err == sql.ErrNoRows:
+	case err != nil:
+		fmt.Println("Failed to get results" ,err)
+	default:
+        fmt.Println(xCoordinate, yCoordinate)
+	}
+	
+	// try making http request against sample site
 	response, err := http.Get("http://ip.jsontest.com/")
 	if err != nil {
 		fmt.Println("Error making GET call")
@@ -26,4 +43,11 @@ func main() {
 		}
 		
 	}
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
+		w.WriteJson(map[string]string{"Body": "Figher is running!"})
+	}))
+
+	fmt.Println(http.ListenAndServe(":"+os.Getenv("PORT"), api.MakeHandler()))
 }
